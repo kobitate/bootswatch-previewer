e98e6a3b783ff8bbf28967b2d0f82b09fc491401
Kobi Tate <kobitate94@gmail.com>
2015-05-02 18:50:33 -0400

    Moved and renamed file for organization

--- target_page_inject.js
+++
@@ -1,6 +0,0 @@
-var sheet = document.getElementById("bootswatch-style");
-if (sheet == null) {
-	var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-	headHTML    += '<link id="bootswatch-style" rel="stylesheet" href="" />';
-	document.getElementsByTagName('head')[0].innerHTML = headHTML;
-}
