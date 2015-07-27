9f4b855203bbc642dc8a3b8b1bdbd43035772d33
Kobi Tate <kobitate94@gmail.com>
2015-07-26 21:57:24 -0400

    Fixed issue with pages not allowing BSPr functionality
    Fixed #22

--- js/theme_functions.js
+++ js/theme_functions.js
@@ -2,11 +2,16 @@
 var themes = ['cerulean','cosmo','cyborg','darkly','flatly','journal','lumen','paper','readable','sandstone','simplex','slate','spacelab','superhero','united','yeti'];
 
 function prepPage() {
-	chrome.tabs.executeScript(null, {file: "target_page/inject.js"});
+	chrome.tabs.executeScript(null, {file: "target_page/inject.js"}, function(){
+		if (chrome.runtime.lastError != undefined) {
+			dialogEvent("page-not-allowed");
+			$("#page-not-allowed").show();
+		}
+	});
 }
 
 function setTheme(t) {
-			
+
 	var themeFile = chrome.extension.getURL('themes/'+t+'.css');
 	chrome.tabs.executeScript(null, {
 		code: 'document.getElementById("bootswatch-style").setAttribute("href", "'+ themeFile +'");'+"\n"+
@@ -15,18 +20,18 @@ function setTheme(t) {
 			'document.getElementById("last-theme").value = \''+ t +'\';\n'+
 			'cacheControl();'
 	});
-	
+
 	$("#download").attr('href', "http://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/"+ t +"/bootstrap.min.css").data('galabel', 'Download: ' + t );
-	
+
 	checkFaved(t, function(isFaved){
 		if (isFaved) {
 			$("#favorite i").addClass("md-favorite").removeClass("md-favorite-outline");
-		} 
+		}
 		else {
 			$("#favorite i").removeClass("md-favorite").addClass("md-favorite-outline");
 		}
 	});
-	
+
 }
 
 function clearTheme() {
@@ -34,7 +39,7 @@ function clearTheme() {
 	//$("#theme-specific-actions").hide();
 	$('.theme-icon.active').toggleClass('active');
 	$("#theme-specific-actions .button").addClass('disabled');
-	
+
 	chrome.tabs.executeScript(null, {
 		code: 'document.getElementById("last-theme").value = \'\';'
 	});
@@ -43,10 +48,10 @@ function clearTheme() {
 function loadThemes() {
 	$("#no-bootstrap").hide();
 	$("#theme-actions").show();
-	
+
 	for (var t in themes) {
 		t = themes[t];
-			
+
 		$("#themes").append(''+
 			'<div class="theme-block" id="theme-' + t + '" data-theme="' + t + '">'+
 				'<div class="theme-icon">'+
@@ -58,13 +63,13 @@ function loadThemes() {
 					'<div class="color color-half danger"></div>'+
 				'</div>' +
 				'<div class="theme-name">'+
-					t + 
+					t +
 				'</div>'+
 			'</div>');
-		
-		
+
+
 	}
-	
+
 	$(".theme-block").click(function(){
 		var t = $(this).data('theme');
 		setTheme(t);
@@ -83,31 +88,31 @@ function loadThemes() {
 			}
 		});
 	});
-	
+
 	getStartpage(function(startupMode){
 		if (startupMode == "favorites") {
 			var startSection = $("section.active");
-		
+
 			if (startSection.attr('id') !== "section-themes") {
 				$("section.active").removeClass('active');
 			}
-			
+
 			$("#section-themes").addClass('active');
 			$("#theme-actions").show();
-			
+
 			$("#themes .theme-block:not(.theme-faved)").hide();
-			
+
 			$("menu ul li[data-target=themes]").removeClass('active');
 			$("menu ul li[data-target=faves]").addClass('active');
-			
+
 			if ($(".theme-block.theme-faved").length === 0) {
 				$("#no-faves").show();
 			}
-			
+
 			$("#startup-select :nth-child(2)").prop('selected', true);
 		}
 	});
-	
+
 	checkAnalyticsOptout(function(optout){
 		$("#card-analytics input[type=checkbox]").prop("checked", optout);
 	});
--- popup/popup.js
+++ popup/popup.js
@@ -18,7 +18,7 @@ function snackbar(text){
 		clearTimeout(closeSnackbar);
 		closeSnackbar = setTimeout(function(){
 			$("#snackbar").removeClass('open');
-		}, 3000); 
+		}, 3000);
 	}
 	else {
 		setTimeout(function(){
@@ -31,160 +31,164 @@ function snackbar(text){
 }
 
 $(document).ready(function(){
-	
+
 	prepPage();
-	
+
 	$(".dialog").hide();
-	
+
 	$("a").click(function(){
 		var href = $(this).attr('href');
 		var gaLabel = $(this).data('galabel') ? $(this).data('galabel') : $(this).text();
-		
+
 		if (href !== "#") {
 			linkEvent(gaLabel);
 			chrome.tabs.create({url: $(this).attr('href')});
 			return false;
 		}
-		
+
 	});
-	
+
 	chrome.runtime.onMessage.addListener(function(message){
 		if (message.method === "bootstrapStatus") {
 			var hasBootstrap = (message.title === "true") ? true : false;
-			
+
 			loadThemes();
-			
+
 			if (!hasBootstrap) {
 				$("#no-bootstrap").show();
 				dialogEvent("no-bootstrap");
 			}
-			
+
 		}
-		
+
 		if (message.method === "themeAlreadySet") {
 			$("#no-bootstrap").hide();
 			if (message.title !== "") {
 				$("#theme-" + message.title + " .theme-icon").addClass('active');
 				$("#theme-specific-actions .button").removeClass('disabled');
 				//$("#theme-specific-actions").show();
-				
+
 				checkFaved(message.title, function(isFaved){
 					if (isFaved) {
 						$("#favorite i").addClass("md-favorite").removeClass("md-favorite-outline");
 					}
 				});
 			}
-			
+
 		}
 	});
-	
+
 	$("#load-anyway").click(function(){
 		$("#reset,#theme-actions").show();
 		$("#no-bootstrap").hide();
 		dialogButton("no-bootstrap: continue");
 	});
-	
+
 	$("#load-anyway-cancel").click(function(){
 		clearTheme();
 		dialogButton("no-bootstrap: cancel");
 		window.close();
 	});
-	
+
+	$("#not-allowed-close").click(function(event) {
+		window.close();
+	});
+
 	$("#reset").click(function(){
 		clearTheme();
 		themeEvent("reset","");
 	});
-	
+
 	$("#hamburger").click(function(){
 		$("menu").toggleClass("open");
 		$("#menu-shade").fadeIn();
 	});
-	
+
 	$("#menu-shade").click(function(){
 		$("menu").toggleClass("open");
 		$("#menu-shade").fadeOut();
 	});
-	
+
 	$("menu ul li:not([data-target=faves]):not(.menu-divider)").click(function(){
 		$("section.active").removeClass('active');
 		$("#themes .theme-block").show();
-		
+
 		$("#no-faves").hide();
-		
+
 		$("menu ul li.active").removeClass('active');
 		$(this).addClass('active');
-		
+
 		var targetName = $(this).data("target");
-		
+
 		var target = $("section#section-" + targetName);
 		target.addClass('active');
-		
+
 		$("menu").toggleClass("open");
 		$("#menu-shade").fadeOut();
-		
+
 		if (targetName !== "themes") {
 			$("#theme-actions").hide();
 		}
 		else {
 			$("#theme-actions").show();
 		}
-		
+
 		tabEvent(targetName);
-		
+
 	});
-	
+
 	$("menu ul li[data-target=faves]").click(function(){
 		var startSection = $("section.active");
-		
+
 		if (startSection.attr('id') !== "section-themes") {
 			$("section.active").removeClass('active');
 		}
-		
+
 		$("#section-themes").addClass('active');
 		$("#theme-actions").show();
-		
+
 		$("#themes .theme-block:not(.theme-faved)").hide();
-		
+
 		$("menu ul li.active").removeClass('active');
 		$(this).addClass('active');
-		
+
 		$("menu").toggleClass("open");
 		$("#menu-shade").fadeOut();
-		
+
 		if ($(".theme-block.theme-faved").length === 0) {
 			$("#no-faves").show();
 		}
-		
+
 		tabEvent("faves");
-		
+
 	});
-	
-	
+
+
 	$("#favorite").click(function(){
 		var item = $(".theme-block .theme-icon.active").parent();
 		var t = item.data('theme');
-		
+
 		item.toggleClass("theme-faved");
-		
+
 		toggleFave(t);
 		$("#favorite i").toggleClass("md-favorite").toggleClass("md-favorite-outline");
-		
+
 		if ($("menu ul li[data-target=faves]").hasClass('active')) {
 			$("#theme-specific-actions .button").addClass('disabled');
 		}
-		
+
 	});
-	
+
 	$("#fave-clear").click(function(){
 		$("#fave-clear-verify").show();
 		dialogEvent("clear-faves");
 	});
-	
+
 	$("#fave-clear-no").click(function(){
 		$("#fave-clear-verify").hide();
 		dialogButton("clear-faves: no");
 	});
-	
+
 	$("#fave-clear-yes").click(function(){
 		clearFaves();
 		$("#fave-clear-verify").hide();
@@ -192,18 +196,18 @@ $(document).ready(function(){
 		snackbar("Favorites Cleared!");
 		dialogButton("clear-faves: yes");
 	});
-	
+
 	$("#startup-select").change(function(){
 		var mode = $(this).val().toLowerCase().replace(" ", "-");
 		chrome.storage.sync.set({startupmode: mode});
 		settingEvent("startup", mode);
 	});
-	
+
 	$("#card-analytics input[type=checkbox]").change(function(event) {
 		var checked = $(this).prop('checked');
 		chrome.storage.sync.set({analyticsoptout: checked});
 		settingEvent("analytics opt out", (checked?"enable":"disable"));
 		sendAnalytics = !checked;
 	});
-	
+
 });
